de678b24fd8ec80dc9fc1bd5a13a2f6b05a052ce
LeoKnox <LeoKnox@users.noreply.github.com>
2021-01-31 23:00:18 -0800

    Add files via upload

--- backend/urls.py
+++ backend/urls.py
@@ -14,7 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from room import views
+
+router = routers.DefaultRouter()
+router.register(r'rooms', views.RoomView, 'room')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
