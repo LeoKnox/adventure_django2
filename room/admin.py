1ccc9f8f9ae66df502ba69485f0ecfaaf0c6abf3
LeoKnox <LeoKnox@users.noreply.github.com>
2021-02-01 22:04:50 -0800

    Add files via upload

--- room/admin.py
+++ room/admin.py
@@ -4,4 +4,4 @@ from .models import Room
 class RoomAdmin(admin.ModelAdmin):
     list_display = ('name', 'descript', 'floor', 'width', 'length')
 
-admind.site.register(Room, RoomAdmin)
+admin.site.register(Room, RoomAdmin)
