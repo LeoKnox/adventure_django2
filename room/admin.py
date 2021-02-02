6511d4dec7826b60a12ea072c548ca96b762c8e0
LeoKnox <LeoKnox@users.noreply.github.com>
2021-02-01 22:09:06 -0800

    Add files via upload

--- room/admin.py
+++ room/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Room
 
 class RoomAdmin(admin.ModelAdmin):
-    list_display = ('name', 'descript', 'floor', 'width', 'length')
+    list_display = ('name', 'description', 'floor', 'width', 'length')
 
 admin.site.register(Room, RoomAdmin)
