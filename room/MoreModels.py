0f8fdeb4ae7f8d6362401a971546e01a43bed585
LeoKnox <LeoKnox@users.noreply.github.com>
2021-02-10 23:43:37 -0800

    Add files via upload

--- room/MoreModels.py
+++ room/MoreModels.py
@@ -1,5 +1,11 @@
 from django.db import models
 
 class MyDungeon(models.Model):
-    dungeon_id = models.IntField(blank=False, primary_key=True)
+    dungeon_id = models.IntegerField(blank=False, primary_key=True)
     dungeon_name = models.CharField(max_length=20)
+
+class MyRoom(models.Model):
+    room_id = models.IntegerField(blank=False, primary_key=True)
+    room_name = models.CharField(max_length=20)
+    width = models.IntegerField()
+    length = models.IntegerField()
