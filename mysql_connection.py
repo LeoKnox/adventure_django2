cbdfd729d736618ff39fb6d9930fdfffaa967425
LeoKnox <LeoKnox@users.noreply.github.com>
2021-02-11 20:45:16 -0800

    Add files via upload

--- mysql_connection.py
+++ mysql_connection.py
@@ -3,14 +3,15 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="passroot"
+    password="passroot",
+    database="mydatabase"  # if database doesn't exist an error will occur
 )
 
 mycursor = mydb.coursor()
 
 # mycursor.execute("CREATE DATA mydatabase")
 
-mycursor.execute("SHOW DATABASES")
+#mycursor.execute("SHOW DATABASES")
 
-for x in mycursor:
-    print(x)
+#for x in mycursor:
+#    print(x)
