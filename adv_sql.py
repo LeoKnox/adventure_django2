f3fe35b4c8817a070fccba55324408cdc2af2f59
LeoKnox <LeoKnox@users.noreply.github.com>
2021-02-17 23:37:54 -0800

    Add files via upload

--- adv_sql.py
+++ adv_sql.py
@@ -9,12 +9,16 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
+mycursor.execute("ALTER TABLE rooms ADD COLUMN width INT, ADD COLUMN length INT")
+
+'''
 sql = "INSERT INTO rooms (name, description) VALUES (%s, %s)"
 val = ("Entry", "Entry way")
 
 mycursor.execute(sql, val)
 
 mydb.commit()
+'''
 
 #mycursor.execute("ALTER TABLE rooms ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
 
