b20bb514cce588c27ad1234b86e587f2032a2d73
LeoKnox <LeoKnox@users.noreply.github.com>
2021-02-19 21:33:05 -0800

    Add files via upload

--- room/views.py
+++ room/views.py
@@ -8,12 +8,19 @@ def home(request):
     context = {'form': PostForm()}
     return render(request, 'room/templates/index.html', context)
 
-@api_view(['GET'])
+@api_view(['GET'], 'POST')
 def room_collection(request):
     if request.method == 'GET':
         rooms = Room.objects.all()
         serializer = RoomSerializer(rooms, many=True)
         return Response(serializer.data)
+    elif request.method == 'POST':
+        data = {'name': request.DATA.get('the_room'), 'description': request.description.pk}
+        serializer = RoomSerlializer(data=data)
+        if serializer.isvalid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 def room_element(request, pk):
