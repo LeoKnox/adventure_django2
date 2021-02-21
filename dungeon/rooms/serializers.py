fe7fd9c1266aedf95361ec0a88fa13b945e89f85
LeoKnox <LeoKnox@users.noreply.github.com>
2021-02-20 22:22:53 -0800

    Add files via upload

---
+++ dungeon/rooms/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from rooms.models import Room
+
+class RoomSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=50)
+    description = serializers.CharField(max_length=255)
+    width = serializers.IntegerField()
+    length = serializers.IntegerField()
+
+def create(self, validated_data):
+    return Room.ojbects.create(**validated_data)
+
+def update(self, instance, validated_data):
+    instance.name = validated_data.get('name', instance.name)
+    instance.description = validated_data.get('description', instance.description)
+    instance.width = validated_data.get('width', instance.width)
+    instance.length = validated_data.get('length', instance.length)
+    instance.save()
+    return instance
