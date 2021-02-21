86baeaaac577215066f938ab5601fb36f794ad51
LeoKnox <LeoKnox@users.noreply.github.com>
2021-02-20 22:07:59 -0800

    Add files via upload

--- dungeon/rooms/models.py
+++ dungeon/rooms/models.py
@@ -1,1 +1,7 @@
-a
+from django.db import models
+
+class Room(models):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+    width = models.IntegerField()
+    length = models.IntegerField()
