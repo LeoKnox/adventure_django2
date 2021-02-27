7f14708fafe9f6923b77e8530bf48ee536fbbacb
LeoKnox <LeoKnox@users.noreply.github.com>
2021-02-26 20:55:06 -0800

    Add files via upload

--- serializers.py
+++ serializers.py
@@ -1,10 +1,25 @@
 from rest_framework import serializers
 from .models import Room
 
-class RoomSerializer(serializers.ModelSerializer):
-    dungeo = serializers.SlurRelatedField(
-        queryset=Dungeon.objects.all(), slug_field='dungeonname'
-    )
+class RoomSerializer(serializers.Serializer):
     class Meta:
         model = Room
-        fields = ('id', 'name', 'description', 'floor', 'width', 'length')
+        fields = ('name', 'description', 'width', 'length')
+    '''
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=50)
+    description = serializers.CharField(max_length=255)
+    width = serializers.IntegerField()
+    length = serializers.IntegerField()
+    '''
+
+def create(self, validated_data):
+    return Room.ojbects.create(**validated_data)
+
+def update(self, instance, validated_data):
+    instance.name = validated_data.get('name', instance.name)
+    instance.description = validated_data.get('description', instance.description)
+    instance.width = validated_data.get('width', instance.width)
+    instance.length = validated_data.get('length', instance.length)
+    instance.save()
+    return instance
