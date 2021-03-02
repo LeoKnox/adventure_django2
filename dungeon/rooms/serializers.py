6c250f9801e58181c611201169bb83fe3167a00e
LeoKnox <LeoKnox@users.noreply.github.com>
2021-03-01 20:44:44 -0800

    Add files via upload

--- dungeon/rooms/serializers.py
+++ dungeon/rooms/serializers.py
@@ -3,18 +3,18 @@ from rooms.models import Room
 
 class RoomSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=50)
-    description = serializers.CharField(max_length=255)
-    width = serializers.IntegerField()
-    length = serializers.IntegerField()
+    name = serializers.CharField(max_length = 50)
+    description = serializers.CharField()
+    width = Serializers.IntegerField()
+    height = Serializers.IntegerField()
 
-def create(self, validated_data):
-    return Room.ojbects.create(**validated_data)
-
-def update(self, instance, validated_data):
-    instance.name = validated_data.get('name', instance.name)
-    instance.description = validated_data.get('description', instance.description)
-    instance.width = validated_data.get('width', instance.width)
-    instance.length = validated_data.get('length', instance.length)
-    instance.save()
-    return instance
+    def create(self, validated_data):
+        return Room.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.width = validated_data.get('width', instance.width)
+        instance.height = validated_data.get('height', instance.height)
+        instance.save()
+        return instance
