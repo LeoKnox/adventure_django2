c7ddd276098323215c34b667db3830e0721249f9
LeoKnox <LeoKnox@users.noreply.github.com>
2021-03-01 20:36:43 -0800

    Add files via upload

--- room/models.py
+++ room/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
 class Room(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length = 50)
     description = models.TextField()
-    floor = models.CharField(max_length=50)
     width = models.IntegerField()
-    length = models.IntegerField()
+    height = models.IntegerField()
 
-    def _str_(self):
-        return self.name
+    class Meta:
+        ordering = ['name']
