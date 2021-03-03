2bf1c6386e069238b54e54dd8c86146267fbbeee
LeoKnox <LeoKnox@users.noreply.github.com>
2021-03-02 20:44:39 -0800

    Add files via upload

--- room/serializers.py
+++ room/serializers.py
@@ -1,7 +1,27 @@
 from rest_framework import serializers
-from .models import Room
+from rooms.models import Room
 
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
-        fields = ('id', 'name', 'description', 'floor', 'width', 'length')
+        fields = ['id', 'name', 'description', 'width', 'length']
+
+'''
+class RoomSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length = 50)
+    description = serializers.CharField()
+    width = Serializers.IntegerField()
+    height = Serializers.IntegerField()
+
+    def create(self, validated_data):
+        return Room.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.width = validated_data.get('width', instance.width)
+        instance.height = validated_data.get('height', instance.height)
+        instance.save()
+        return instance
+        '''
