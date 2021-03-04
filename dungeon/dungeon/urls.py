97f8806f1a86dfd4980ec6a210a767597f4204b0
LeoKnox <LeoKnox@users.noreply.github.com>
2021-03-03 20:38:00 -0800

    Add files via upload

--- dungeon/dungeon/urls.py
+++ dungeon/dungeon/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('rooms.urls')),
+    path('admin/', admin.site.urls),
+    path('rooms/<str:name>/', room.views.show, name='show-room'),
+    path('level/', room.views.level, name='level'),
+    path('', room.views.index, name='list-rooms'),
 ]
