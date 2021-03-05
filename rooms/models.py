a84ac84eb656d766eed533551bb6151de8db8088
LeoKnox <LeoKnox@users.noreply.github.com>
2021-03-04 21:21:37 -0800

    Add files via upload

--- rooms/models.py
+++ rooms/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 class Room(models.Model):
+    MONSTERS = [('Orc', 'Orc'), ('Skeleton', 'Skeleton'), ('Dragon', 'Dragon')]
     name = models.CharField(max_length = 50)
     description = models.TextField()
+    monster = models.CharField(max_length = 100, choices=MONSTERS)
     width = models.IntegerField()
     heigh = models.IntegerField()
+    doors = models.ManyToManyField('Door')
+
+class Door(models.Model):
+    next_room = models.CharField(max_length=50)
