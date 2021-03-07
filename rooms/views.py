5860ffe80fcb35de4b646381e2f3ef26cc68ba66
LeoKnox <LeoKnox@users.noreply.github.com>
2021-03-06 20:40:12 -0800

    Add files via upload

--- rooms/views.py
+++ rooms/views.py
@@ -5,11 +5,11 @@ from .models import Room
 
 def home(request):
     rooms = Room.objects.all()
-    return render(request, 'home.html', {'rooms'}: rooms)
+    return render(request, 'home.html', {'rooms': rooms})
 
 def room_detail(request, room_id):
     try:
         room = Room.objects.get(id=room_id)
     except Room.DoesNotExist:
         raise Http404('Room does not exist. Go Build it!')
-    return render(request, room_detail.html', {'room': room})
+    return render(request, 'room_detail.html', {'room': room})
