450c328c3bb28eabad51565a96241f3a06149093
LeoKnox <LeoKnox@users.noreply.github.com>
2021-03-08 20:35:38 -0800

    Add files via upload

--- adventure/urls.py
+++ adventure/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
     path('rooms/<int:room_id>/', views.room_detail, name='room_detail'),
+    path('rooms/upadte/<int:room_id/', views.room_edit, name='room_edit'),
 
 ]
