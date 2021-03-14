8ff29f62d9a2291a7dc0df5809651cbc61da5455
LeoKnox <LeoKnox@users.noreply.github.com>
2021-03-13 20:36:34 -0800

    Add files via upload

--- adventure/urls.py
+++ adventure/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 
 from rooms import views
 
+app_name = 'rooms'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
     path('rooms/edit/<int:room_id>/', views.room_edit, name='room_edit'),
     path('rooms/<int:room_id>/', views.room_detail, name='room_detail'),
     path('rooms/create/', views.room_create, name='room_create'),
+    path('rooms/delete', views.room_delete, name='room_delete'),
 ]
