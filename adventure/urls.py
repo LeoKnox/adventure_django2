4a9ea1e57b55bd98cd9524bdcf98569863980bfe
LeoKnox <LeoKnox@users.noreply.github.com>
2021-03-24 21:10:31 -0700

    Add files via upload

--- adventure/urls.py
+++ adventure/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('rooms/create/', views.room_create, name='room_create'),
     path('rooms/delete/<int:room_id>', views.room_delete, name='room_delete'),
     path('rooms/door_delete/<int:door_id>', views.door_delete, name='door_delete'),
+    path('rooms/door_edit/<int:room_id>', views.door_edit, name='door_edit'),
 ]
