b628b877e18655b40857ea54a40e956ae942fb6b
LeoKnox <LeoKnox@users.noreply.github.com>
2021-04-16 20:35:16 -0700

    Add files via upload

--- rooms/views.py
+++ rooms/views.py
@@ -77,8 +77,10 @@ def room_edit(request, room_id):
         edit_room.save()
         new_door = request.POST.getlist('doors') #doesn't but does now!
         for nd in new_door:
-            single_door = Door(next_room = room_id)
+            single_door = Door(next_room = nd)
             #single_door.save()
+            print(single_door.id)
+            print(single_door.next_room)
             edit_room.doors.add(single_door)
         #next_room = request.POST.get('name') #works
         return redirect('home')
