42a9d17b716a3ba02710aa39bb5867d3f2eb325a
LeoKnox <LeoKnox@users.noreply.github.com>
2021-04-24 19:48:29 -0700

    Add files via upload

--- rooms/views.py
+++ rooms/views.py
@@ -65,7 +65,7 @@ def room_edit(request, room_id):
     shapes = Room.SHAPES
     #print (shapes[1][0]) #use to populate room shape forms
     #doors = Door.objects.all()
-    doors = edit_room.objects.all(doors)
+    doors = edit_room.doors.Door.all()
     print("*******")
     print(doors)
     doors = Door.objects.filter()
