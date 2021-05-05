84b7f6120bac5a3481c3d9674aa57fe243be3e46
LeoKnox <LeoKnox@users.noreply.github.com>
2021-05-05 16:37:02 -0700

    Add files via upload

--- rooms/views.py
+++ rooms/views.py
@@ -95,7 +95,7 @@ def room_edit(request, room_id):
         return redirect('home')
     return render(request, 'room_edit.html', {'edit_room': edit_room, 'doors':doors, 'door_dupe':door_dupe})
 
-def edit_door(request, door_id):
+def edit_door(request, door_id=119):
     print(door_id)
     return render(request, 'edit_door.html')
 
