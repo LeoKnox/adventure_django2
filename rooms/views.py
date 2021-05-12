b8d6ac015e4542a9b8db1eb1ecdf7497c3e2a6c1
LeoKnox <LeoKnox@users.noreply.github.com>
2021-05-11 22:29:24 -0700

    Add files via upload

--- rooms/views.py
+++ rooms/views.py
@@ -64,11 +64,9 @@ def room_edit(request, room_id):
     edit_room = Room.objects.get(pk = room_id)
     shapes = Room.SHAPES
     doors = Room.objects.all()
-    door_dupe = [val for val in Door.objects.all() if val not in edit_room.doors.all()] # delete if other door_dupe later
-    print(door_dupe)
+    doors = [val for val in Door.objects.all() if val not in edit_room.doors.all()] # delete if other door_dupe later
     #for dupe in door_dupe:
         #print(Door.objects.get(next_room = dupe.door))
-    doors = door_dupe
     door_dupe = [val for val in Room.objects.values_list('name', flat=True) if val not in edit_room.doors.values_list('next_room', flat=True)]
     #door_dupe = edit_room.doors.values_list('next_room', flat=True)
     #door_dupe.values_list('next_room', flat=True)
