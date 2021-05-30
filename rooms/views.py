93a6212485f95864aef9a189d34e1998ababa8f7
LeoKnox <LeoKnox@users.noreply.github.com>
2021-05-29 20:31:34 -0700

    Add files via upload

--- rooms/views.py
+++ rooms/views.py
@@ -75,7 +75,10 @@ def room_edit(request, room_id):
         new_door = request.POST.getlist('doors') #doesn't but does now!
         new_door2 = [new_door[x:x+3] for x in range(0, len(new_door), 3) if x != '']
         edit_room.save()
+        print(new_doors2)
         for nd in new_door2:
+            print("!!!!!")
+            print(nd)
             if nd[0] != "":
                 single_door = Door(next_room = nd[0], wall = nd[1], location = nd[2])
             '''
