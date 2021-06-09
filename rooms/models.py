0ed8a98e87b86cfd9d9ae369a45b352666499cca
LeoKnox <LeoKnox@users.noreply.github.com>
2021-06-08 20:39:42 -0700

    Add files via upload

--- rooms/models.py
+++ rooms/models.py
@@ -6,11 +6,13 @@ class Room(models.Model):
     description = models.TextField()
     shape = models.CharField(max_length = 100, choices=SHAPES)
     width = models.IntegerField()
-    heigh = models.IntegerField()
+    height = models.IntegerField()
     doors = models.ManyToManyField('Door')
 
 class Door(models.Model):
     next_room = models.CharField(max_length=50)
+    location = models.IntegerField(null=True)
+    wall = models.IntegerField(null=True)
 
     def __str__(self):
         return self.next_room
